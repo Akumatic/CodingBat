c88e788a10d77182a92455cdc53727595dbe25d9
Akumatic <ugp@hotmail.de>
2019-08-05 19:01:02 +0200

    Added accidentally cut out solutions for WarmUp2

--- Python/exercise/warmup/WarmUp2.py
+++ Python/exercise/warmup/WarmUp2.py
@@ -52,3 +52,77 @@ def string_splosion(str):
     for i in  range(0, len(str)):
         result += str[:i+1]
     return result
+
+''' https://codingbat.com/prob/p145834
+Given a string, return the count of the number of times that a substring length
+2 appears in the string and also as the last 2 chars of the string, so "hixxxhi"
+yields 1 (we won't count the end substring).
+
+last2('hixxhi') → 1
+last2('xaxxaxaxx') → 1
+last2('axxxaaxx') → 2
+'''
+def last2(str):
+    cnt = 0
+    for i in range(0, len(str) - 2):
+        if str[i:i+2] == str[-2:]:
+            cnt += 1
+    return cnt
+
+''' https://codingbat.com/prob/p166170
+Given an array of ints, return the number of 9's in the array.
+
+array_count9([1, 2, 9]) → 1
+array_count9([1, 9, 9]) → 2
+array_count9([1, 9, 9, 3, 9]) → 3
+'''
+def array_count9(nums):
+    cnt = 0
+    for n in nums:
+        if n == 9:
+            cnt += 1
+    return cnt
+
+''' https://codingbat.com/prob/p110166
+Given an array of ints, return True if one of the first 4 elements in the array
+is a 9. The array length may be less than 4.
+
+array_front9([1, 2, 9, 3, 4]) → True
+array_front9([1, 2, 3, 4, 9]) → False
+array_front9([1, 2, 3, 4, 5]) → False
+'''
+def array_front9(nums):
+    for n in nums[:4]:
+        if n == 9:
+            return True
+    return False
+
+''' https://codingbat.com/prob/p193604
+Given an array of ints, return True if the sequence of numbers 1, 2, 3 appears
+in the array somewhere.
+
+array123([1, 1, 2, 3, 1]) → True
+array123([1, 1, 2, 4, 1]) → False
+array123([1, 1, 2, 1, 2, 3]) → True
+'''
+def array123(nums):
+    for i in range(0, len(nums) - 2):
+        if nums[i:i+3] == [1, 2, 3]:
+            return True
+    return False
+
+''' https://codingbat.com/prob/p182414
+Given 2 strings, a and b, return the number of the positions where they contain
+the same length 2 substring. So "xxcaazz" and "xxbaaz" yields 3, since the "xx",
+"aa", and "az" substrings appear in the same place in both strings.
+
+string_match('xxcaazz', 'xxbaaz') → 3
+string_match('abc', 'abc') → 2
+string_match('abc', 'axc') → 0
+'''
+def string_match(a, b):
+    cnt = 0
+    for i in range(0, len(a) - 1 if len(a) < len(b) else len(b) - 1):
+        if a[i:i+2] == b[i:i+2]:
+            cnt += 1
+    return cnt
